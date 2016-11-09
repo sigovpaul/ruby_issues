09d952888a356a65e3f5e207d1407821782207a8
Sigov Paul <sigovpaul@gmail.com>
2016-11-09 23:35:49 +0300

    2nd lesson

---
+++ lesson_2/array.rb
@@ -0,0 +1,3 @@
+array = []
+10.step(by: 5, to: 100){|x| array.push(x)}
+puts array
---
+++ lesson_2/fibonacci.rb
@@ -0,0 +1,9 @@
+array = [0,1]
+i = 2
+loop do 
+	new_element = array[i - 1] + array[i - 2]
+break if new_element > 100
+	array.push(new_element)
+	i += 1
+end
+puts array 
