a128c03385135041a0bb7438ce3595a3cb52634d
Sigov Paul <sigovpaul@gmail.com>
2016-11-13 23:28:39 +0300

    2nd lesson

--- lesson_2/month.rb
+++ lesson_2/month.rb
@@ -1,4 +1,4 @@
 month = {"january" => 31, "february" => 29, "march" => 31, "april" => 30, "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, "october" => 31, "november" => 30, "december" => 31}
 month.each_pair do |name, days|
-	puts name if month[name] == 30
+puts name if month[name] == 30
 end
--- lesson_2/products.rb
+++ lesson_2/products.rb
@@ -2,7 +2,6 @@ product_info = {}
 product_cost = {}
 total = 0
 loop do
-  puts "введите название товара"
   name = gets.chomp
   break if name == "стоп"
   puts "введите цену товара"
@@ -10,10 +9,10 @@ loop do
   puts "введите кол-во товара"
   quantity = gets.chomp.to_f
   if product_info.has_key?(name)
-  	product_info[name].store(price, quantity)
+    product_info[name].store(price, quantity)
   else
-		product_info[name] = {price => quantity}
-	end
+    product_info[name] = {price => quantity}
+  end
 
 puts product_info
 product_cost[name] = price * quantity
