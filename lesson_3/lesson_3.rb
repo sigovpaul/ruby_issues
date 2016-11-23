b11df0f2ca3c677c3fbece13daa8bae436b98c16
Sigov Paul <sigovpaul@gmail.com>
2016-11-24 00:48:50 +0300

    3rd lesson

--- lesson_3/lesson_3.rb
+++ lesson_3/lesson_3.rb
@@ -1,8 +1,24 @@
+=begin
+Класс Station (Станция):
+Имеет название, которое указывается при ее создании
+Может принимать поезда (по одному за раз)
+Может показывать список всех поездов на станции, находящиеся в текущий момент
+Может показывать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
+Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+=end
+
 class Station
-end
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def arrived_train(train)
+    @trains_list << train
+  end
+  
+  def show_trains_list(trains_list)
+    @trains_list.each {|i| puts trains_list}
+  end
 
-class Route
-end
-
-class Train
 end
